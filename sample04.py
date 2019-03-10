cd4729151a7d60814def55a6d7267c32df145f4d
Sato <s_ychm@yahoo.co.jp>
2019-03-10 11:13:53 +0900

    関数いろいろ定義

---
+++ sample04.py
@@ -0,0 +1,13 @@
+def is_even(x: int) -> bool:
+    return x % 2 == 0
+
+
+def is_odd(x: int) -> bool:
+    return x % 2 != 0
+
+
+print(is_even(x=3))
+print(is_even(x=4))
+
+print(is_odd(x=3))
+print(is_odd(x=4))
