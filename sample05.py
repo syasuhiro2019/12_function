3f7e98e6685656c87420f84c6c513a97c5324bad
Sato <s_ychm@yahoo.co.jp>
2019-03-10 12:38:59 +0900

    2つの整数の和と差を計算する関数

--- sample05.py
+++ sample05.py
@@ -4,17 +4,15 @@ def add(a: int, b: int) -> int:
     return a + b
 
 
-y = add(a=3, b=4)
-
-print(y)  # ここで7が出力されればok
-
-
-# 2つの整数の差を返す関数を定義
-
 def diff(a: int, b: int) -> int:
     return a - b
 
 
-y2 = diff(a=3, b=4)
+if __name__ == '__main__':
+    y = add(a=3, b=4)
+    print(y)  # ここで7が出力されればok
 
-print(y2)
+    # 2つの整数の差を返す関数を定義
+
+    y2 = diff(a=3, b=4)
+    print(y2)
---
+++ smple06.py
@@ -0,0 +1,8 @@
+import sample05
+
+if __name__ == '__main__':
+    y = sample05.add(a=35, b=8)
+    print(y)
+
+    y2 = sample05.diff(33, 8)
+    print(y2)
