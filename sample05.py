9f3f0a421cd5cb6bcb7c757e7287b8d2d5b510d2
Sato <s_ychm@yahoo.co.jp>
2019-03-10 11:48:45 +0900

    2つの整数の差を計算する関数

--- sample05.py
+++ sample05.py
@@ -7,3 +7,14 @@ def add(a: int, b: int) -> int:
 y = add(a=3, b=4)
 
 print(y)  # ここで7が出力されればok
+
+
+# 2つの整数の差を返す関数を定義
+
+def diff(a: int, b: int) -> int:
+    return a - b
+
+
+y2 = diff(a=3, b=4)
+
+print(y2)
